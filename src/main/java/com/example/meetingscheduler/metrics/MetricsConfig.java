c2883143d5af0c2d6d0e082cf7c760e18981ddfc
Waska <91632924+waseemukhan@users.noreply.github.com>
2026-04-05 17:16:53 +0200

    Update MetricsConfig.java

--- src/main/java/com/example/meetingscheduler/metrics/MetricsConfig.java
+++ src/main/java/com/example/meetingscheduler/metrics/MetricsConfig.java
@@ -1,1 +1,18 @@
 
+package com.example.meetingscheduler.metrics;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+
+    @Bean
+    public Counter meetingsCreatedCounter(MeterRegistry registry) {
+        return Counter.builder("meetingscreatedtotal")
+                .description("Total meetings created")
+                .register(registry);
+    }
+}
