0f7ca7cbd7b1952130e828800209443cf4ddfc1a
Waska <91632924+waseemukhan@users.noreply.github.com>
2026-04-05 17:00:47 +0200

    Update MeetingSchedulerApplication.java

--- src/main/java/com/example/meetingscheduler/MeetingSchedulerApplication.java
+++ src/main/java/com/example/meetingscheduler/MeetingSchedulerApplication.java
@@ -1,1 +1,15 @@
 
+
+`java
+package com.example.meetingscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingSchedulerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingSchedulerApplication.class, args);
+    }
+}
+`
