f6f3c86c8ef8f5d4207ae1006f76d393e99da4d9
Waska <91632924+waseemukhan@users.noreply.github.com>
2026-04-05 17:31:33 +0200

    Update MeetingRepository.java

--- src/main/java/com/example/meetingscheduler/repository/MeetingRepository.java
+++ src/main/java/com/example/meetingscheduler/repository/MeetingRepository.java
@@ -1,1 +1,8 @@
 
+import com.example.meetingscheduler.domain.Meeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MeetingRepository extends JpaRepository<Meeting, UUID> {
+}
