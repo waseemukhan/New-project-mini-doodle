2f6679c7910c44d18cd6f7d110fa7d045130f7a1
Waska <91632924+waseemukhan@users.noreply.github.com>
2026-04-05 17:13:14 +0200

    Update MeetingResponse.java

--- src/main/java/com/example/meetingscheduler/web/dto/MeetingResponse.java
+++ src/main/java/com/example/meetingscheduler/web/dto/MeetingResponse.java
@@ -1,1 +1,9 @@
 
+public class MeetingResponse {
+    private String id;
+    private String slotId;
+    private String title;
+    private String description;
+    private List<MeetingRequest.ParticipantDto> participants;
+    // getters/setters
+}
